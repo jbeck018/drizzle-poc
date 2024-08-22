d1e2b873d868516eee1abe5a37809e45694e38e9
Jacob Beck <jacob@parlor.io>
2024-08-22 14:45:43 -0700

    fix redirescts

--- app/routes/_home+/_index.tsx
+++ app/routes/_home+/_index.tsx
@@ -1,7 +1,9 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
-// dashboard/index.tsx
 import { authenticator } from "#app/modules/auth/auth.server";
+// dashboard/index.tsx
+import { ROUTE_PATH as DASHBOARD } from "../dashboard+/_layout";
+import { ROUTE_PATH as ONBOARDING } from '../onboarding+/username';
 
 export const ROUTE_PATH = '/' as const
 
@@ -9,5 +11,11 @@ export const loader: LoaderFunction = async ({ request }) => {
   const sessionUser = await authenticator.isAuthenticated(request)
   if (!sessionUser) {
     return redirect(`/auth/login`);
+  }
+
+  if (sessionUser.username) {
+    return redirect(DASHBOARD)
+  } else {
+    return redirect(ONBOARDING);
   }
 };
