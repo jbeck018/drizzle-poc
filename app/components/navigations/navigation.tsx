9d5d44872971e4de48da1d5b8a01ef0936febc0d
Jacob Beck <jacob@parlor.io>
2024-08-22 14:56:14 -0700

    add logout

--- app/components/navigations/navigation.tsx
+++ app/components/navigations/navigation.tsx
@@ -1,6 +1,8 @@
-import { Flex } from '@chakra-ui/react';
-import { Outlet, useLocation } from "@remix-run/react";
+import { Flex, Icon } from '@chakra-ui/react';
+import { Outlet, useLocation, useSubmit } from "@remix-run/react";
 import {AnimatePresence, motion} from 'framer-motion';
+import { LogOut } from 'lucide-react';
+import { ROUTE_PATH as LOGOUT_PATH } from '#app/routes/auth+/logout'
 import { Container, Nav, NavItem, Page } from './shared-components';
 import { IconItem } from './types';
 
@@ -8,6 +10,7 @@ const MotionOutlet = motion(Outlet);
 
 export const Navigation = ({ topItems, bottomItems }: { topItems: IconItem[]; bottomItems: IconItem[] }) => {
     const location = useLocation();
+    const submit = useSubmit()
     return (
         <Container>
             <Nav>
@@ -16,6 +19,7 @@ export const Navigation = ({ topItems, bottomItems }: { topItems: IconItem[]; bo
                 </Flex>
                 <Flex direction='column' gap={5} marginBottom={5}>
                     {bottomItems.map(item => NavItem(item))}
+                    <Icon height={8} width={8} as={LogOut} onClick={() => submit({}, { action: LOGOUT_PATH, method: 'POST' })} />
                 </Flex>
             </Nav>
             <Page>
