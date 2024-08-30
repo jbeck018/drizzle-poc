a65a9913dbee2659391ca575a880ef5884aaa24d
Jacob Beck <jacob@parlor.io>
2024-08-30 09:34:08 -0700

    use field all

--- temporal/data-syncing/utils/salesforce-bulk-api.ts
+++ temporal/data-syncing/utils/salesforce-bulk-api.ts
@@ -3,6 +3,6 @@ import { Readable } from 'stream';
 import csv from 'csv-parser';
 
 export async function streamBulkQuery(connection: Connection, objectType: string): Promise<Readable> {
-  const stream = await connection.bulk2.query(`SELECT Id, Name FROM ${objectType}`);
+  const stream = await connection.bulk2.query(`SELECT FIELDS(ALL) FROM ${objectType}`);
   return stream.pipe(csv());
 }
